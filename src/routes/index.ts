e3aea7e4f895073513177bf3d5b896a28f1694cd
JoShMiQueL <joshmiquel.cs@gmail.com>
2021-07-14 14:10:48 +0200

    Complete

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import ExerciseRoute from "./exercise/Exercises.routes";
+import path from "path";
+import root from "../../root";
 
 const app = express();
 
 app.set("port", process.env.SERVER_PORT);
-
+app.use(express.static(path.join(root, "public")));
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
