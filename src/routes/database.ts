09090ab7186ac972d586b44e5abd28608f34ca4c
JoShMiQueL <joshmiquel.cs@gmail.com>
2021-07-15 16:15:59 +0200

    Refactoring

--- src/routes/database.ts
+++ src/routes/database.ts
@@ -1,4 +1,5 @@
 import chalk from "chalk";
+import _ from "lodash";
 import mongoose, { ConnectOptions } from "mongoose";
 const { MONGO_DATABASE, MONGO_HOST, MONGO_USER, MONGO_PASSWORD } = process.env;
 
@@ -13,14 +14,33 @@ const { MONGO_DATABASE, MONGO_HOST, MONGO_USER, MONGO_PASSWORD } = process.env;
     MONGO_PASSWORD ? `${MONGO_USER}:${MONGO_PASSWORD}@` : ""
   }${MONGO_HOST}/${MONGO_DATABASE}`;
 
-  try {
-    const db = await mongoose.connect(mongooseURL, mongooseOptions);
-    mongoose.Promise = global.Promise;
+  const connectDB = async () => {
+    let connected = false;
+    let db: typeof mongoose;
+    while (!connected) {
+      try {
+        db = await mongoose.connect(mongooseURL, mongooseOptions);
+        if (db.connection) {
+          connected = true;
+          continue;
+        }
+        // mongoose.Promise = global.Promise;
+        await connectDB();
+      } catch (error) {
+        // console.error(error);
+        console.log(
+          chalk`{blue Database} - Could not connect to {yellow ${mongooseURL}}, reconnecting...`
+        );
+        connected = false;
+        mongoose.connection.close();
+      }
+    }
     console.log(
-      chalk`{blue Database} {white - Connected to {yellow ${mongooseURL}} {white -} {yellow ${db.connection.name}} database}`
+      chalk`{blue Database} - Connected to {yellow ${mongooseURL}} - {yellow ${
+        db!.connection.name
+      }} database`
     );
-  } catch (error) {
-    console.error(error);
-    mongoose.connection.close();
-  }
+  };
+
+  await connectDB();
 })();
